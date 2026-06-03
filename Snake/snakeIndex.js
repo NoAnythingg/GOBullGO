979a396d4acabdf0ab8ccea83a31ccb7cc8f60cd
millie-school-acc <amir.shaide@realgymnasiet.se>
2026-06-03 08:42:47 +0200

    ALL files pushed

---
+++ Snake/snakeIndex.js
@@ -0,0 +1,17 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
